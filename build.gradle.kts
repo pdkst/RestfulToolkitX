bb65600fd6d3aa6b377d4dbe0682b3f806ea50b1
pdkst.zhang <pdkst.zhang@gaodun.com>
2023-08-08 11:47:02 +0800

    add repositories mirror

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,6 +24,10 @@ version = properties("pluginVersion").get()
 
 // Configure project's dependencies
 repositories {
+    maven {
+        setUrl("https://maven.aliyun.com/repository/public/")
+    }
+    mavenLocal()
     mavenCentral()
 }
 
@@ -83,7 +87,7 @@ tasks {
             val start = "<!-- Plugin description -->"
             val end = "<!-- Plugin description end -->"
 
-            with (it.lines()) {
+            with(it.lines()) {
                 if (!containsAll(listOf(start, end))) {
                     throw GradleException("Plugin description section not found in README.md:\n$start ... $end")
                 }
@@ -126,6 +130,10 @@ tasks {
         // The pluginVersion is based on the SemVer (https://semver.org) and supports pre-release labels, like 2.1.7-alpha.3
         // Specify pre-release label to publish the plugin in a custom Release Channel automatically. Read more:
         // https://plugins.jetbrains.com/docs/intellij/deployment.html#specifying-a-release-channel
-        channels.set(properties("pluginVersion").map { listOf(it.split('-').getOrElse(1) { "default" }.split('.').first()) })
+        channels.set(properties("pluginVersion").map {
+            listOf(
+                it.split('-').getOrElse(1) { "default" }.split('.').first()
+            )
+        })
     }
 }
